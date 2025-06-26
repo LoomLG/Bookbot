04cf1efe4264e97bfb99789f2f51c6de2b2d7d50
github_loomLG <tlomen@blc.edu>
2025-06-26 11:19:24 -0500

    5: added sys to main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import number_of_words , count_characters, sorted_list
 
 def get_book_text(filepath):
@@ -6,9 +7,12 @@ def get_book_text(filepath):
     return content
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_text = get_book_text(sys.argv[1])
     report = ("============ BOOKBOT ============\n"
-             "Analyzing book found at books/frankenstein.txt...\n" 
+             f"Analyzing book found at {sys.argv[1]}...\n" 
              "----------- Word Count ----------\n" 
              f"Found {number_of_words(book_text)} total words\n" 
              "--------- Character Count -------")
